8fb24aa4f060deeb754388b10159793f87bdbd1b
Niccolo' Paladino <niccolo.paladino@comm-it.it>
2019-02-28 16:13:28 +0100

    modificato il seed del primo utente per avere tutti i club

--- database/seeds/UserClubTableSeeder.php
+++ database/seeds/UserClubTableSeeder.php
@@ -20,6 +20,14 @@ class UserClubTableSeeder extends Seeder
                 'club_id' => '1',
             ],
             [
+                'user_id' =>'1',
+                'club_id' => '2',
+            ],
+            [
+                'user_id' =>'1',
+                'club_id' => '3',
+            ],
+            [
                 'user_id' =>'2',
                 'club_id' => '1',
             ],
